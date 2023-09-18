b40b05d1681aafdbd50dbeb4cc00959c9a216581
Khloud <62654747+Khloud-M@users.noreply.github.com>
2023-09-18 15:59:17 +0300

    main

--- src/pages/Slider/TheSlider.vue
+++ src/pages/Slider/TheSlider.vue
@@ -1,108 +1,141 @@
 <template>
-  <section>
-  data slder 
-  </section>
+  <div class="mainfilter">
+    <v-breadcrumbs :items="items" divider=">>"></v-breadcrumbs>
+  </div>
+  <div class="filter_add">
+    <h2>
+      <router-link to="/sliders/add">
+        <v-icon size="25"> mdi-plus</v-icon>
+
+        إضافة جديد
+      </router-link>
+    </h2>
+    <v-data-table
+      v-model:page="page"
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="itemsPerPage"
+      hide-default-footer
+      class="elevation-1"
+    >
+      <template v-slot:bottom>
+        <div class="text-center pt-2">
+          <v-pagination v-model="page" :length="pageCount"></v-pagination>
+        </div>
+      </template>
+    </v-data-table>
+  </div>
 </template>
+
 <script>
 export default {
-  data() {
-    return {
-      headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "start",
-          sortable: false,
-          value: "name",
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" },
-      ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: 1,
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: 1,
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: 7,
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: 8,
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: 16,
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: 0,
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: 2,
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: 45,
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: 22,
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: 6,
-        },
-      ],
-    };
+  data: () => ({
+    items: [
+      {
+        title: "الصفحة الرئيسية",
+        disabled: false,
+        href: "/",
+      },
+      {
+        title: "تصنيفات الرئيسية",
+        disabled: false,
+        href: "/mainFilters",
+      },
+      {
+        title: " إضافة تصنيف",
+        disabled: false,
+        href: "mainFilters/add",
+      },
+    ],
+    page: 1,
+    itemsPerPage: 5,
+    headers: [
+      {
+        align: "start",
+        key: "name",
+        sortable: false,
+        title: "الصورة",
+      },
+      { title: " العنوان(عربي)", key: "calories" },
+      { title: "الاسم (عربي)", key: "protein" },
+      { title: "المحتوى (عربي)", key: "fat" },
+      { title: "المحتوى (انجليزي)", key: "iron" },
+      { title: "التحكم", key: "carbs" },
+    ],
+    desserts: [
+      {
+        name: "Gingerbread",
+        calories: 356,
+        fat: 16.0,
+        carbs: 49,
+        protein: 3.9,
+        iron: 16,
+      },
+      {
+        name: "Jelly bean",
+        calories: 375,
+        fat: 0.0,
+        carbs: 94,
+        protein: 0.0,
+        iron: 0,
+      },
+      {
+        name: "Lollipop",
+        calories: 392,
+        fat: 0.2,
+        carbs: 98,
+        protein: 0,
+        iron: 2,
+      },
+      {
+        name: "Honeycomb",
+        calories: 408,
+        fat: 3.2,
+        carbs: 87,
+        protein: 6.5,
+        iron: 45,
+      },
+      {
+        name: "KitKat",
+        calories: 518,
+        fat: 26.0,
+        carbs: 65,
+        protein: 7,
+        iron: 6,
+      },
+    ],
+  }),
+  computed: {
+    pageCount() {
+      return Math.ceil(this.desserts.length / this.itemsPerPage);
+    },
   },
 };
 </script>
+
+<style scoped>
+.mainfilter {
+  margin: 15px auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center !important;
+  border-radius: 15px;
+  background-color: white;
+  box-shadow: var(--main_box_shadow);
+}
+.filter_add {
+  background-color: white;
+  box-shadow: var(--main_box_shadow);
+  border-radius: 10px;
+  padding: 30px 15px;
+}
+h2 {
+  margin: 20px 0;
+  padding: 10px;
+  text-align: left;
+}
+h2 a,
+.v-icon {
+  color: var(--main-color);
+}
+</style>
